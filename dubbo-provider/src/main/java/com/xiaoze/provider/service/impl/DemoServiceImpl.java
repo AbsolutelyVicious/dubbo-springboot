2ac982928744a6cfb529c185f8c0f605780fa98c
xuyuandong <17565912@qq.com>
2020-03-27 23:44:20 +0800

    Initial commit

---
+++ dubbo-provider/src/main/java/com/xiaoze/provider/service/impl/DemoServiceImpl.java
@@ -0,0 +1,19 @@
+package com.xiaoze.provider.service.impl;
+
+import com.xiaoze.api.service.DemoService;
+import org.apache.dubbo.config.annotation.Service;
+
+/**
+ * DemoServiceImpl
+ * 服务提供类
+ * @author xiaoze
+ * @date 2018/6/7
+ */
+@Service(version = "${demo.service.version}")
+public class DemoServiceImpl implements DemoService {
+
+    @Override
+    public String sayHello(String name) {
+        return "Hello, " + name + " (from Spring Boot)";
+    }
+}
